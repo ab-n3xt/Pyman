b8cb4fb5eef8b2b52c8b698f9af5b81723ca891f
Aidan B <aidan.buchanan@gmail.com>
2019-09-18 11:30:18 -0400

    Test file for testing the death animation when Pacman collides with Ghost

--- tests/13 - Death Animation/death.py
+++ tests/13 - Death Animation/death.py
@@ -1,8 +1,6 @@
 import os, sys, pygame
 
 from Pacman import Pacman
-from Pellet import Pellet
-from Magic_Pellet import Magic_Pellet
 from Ghost import Ghost
 
 from pygame.locals import *
@@ -40,17 +38,8 @@ pacman_group = pygame.sprite.GroupSingle(pacman)
 ghost_group = pygame.sprite.Group()
 ghost_group.add(Ghost(300, 300))
 
-# create pellets
-pellet_group = pygame.sprite.Group()
-pellet_group.add(Pellet(50, 50), Pellet(100, 100))
-
-# create magic pellets
-m_pellet_group = pygame.sprite.Group()
-m_pellet_group.add(Magic_Pellet(200, 200))
-
 # blit pacman and pellets to the surface
 pacman_group.draw(windowSurface)
-pellet_group.draw(windowSurface)
 
 # update the game
 pygame.display.update()
@@ -81,32 +70,9 @@ while True:
                 moveRight = False
                 moveDown  = False
                 moveUp    = True
-        if event.type == KEYUP:
-            if event.key == K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-            if event.key == K_LEFT:
-                moveLeft = False
-            if event.key == K_RIGHT:
-                moveRight = False
-            if event.key == K_DOWN:
-                moveDown = False
-            if event.key == K_UP:
-                moveUp = False
                 
     # move the sprite(pacman)
     pacman_group.update(moveLeft, moveRight, moveDown, moveUp)
-    
-    # check if Pacman collided with any Pellets in Pellet Group
-    # true = Pellet will be destroyed when collided with
-    pygame.sprite.spritecollide(pacman, pellet_group, True)
-    
-    # check if Pacman collided with any Magic Pellets
-    # If so, then trigger the ghost's vulnerability
-    collided_mps = pygame.sprite.spritecollide(pacman, m_pellet_group, True)
-    if len(collided_mps) == 1:
-        for ghost in ghost_group:
-            ghost.triggerVulnerability()
     
     # check if Pacman collided with any Ghosts
     # If so, check if they are vulnerable
@@ -117,6 +83,9 @@ while True:
         if ghost.isVulnerable:
             ghost.kill()
         else:
+            windowSurface.fill(BLACK)
+            pygame.display.update()
+            pacman.death()
             pygame.quit()
             sys.exit()
             
@@ -127,8 +96,6 @@ while True:
     windowSurface.fill(BLACK)
     pacman_group.draw(windowSurface)
     ghost_group.draw(windowSurface)
-    pellet_group.draw(windowSurface)
-    m_pellet_group.draw(windowSurface)
     
     # update the game
     pygame.display.update()
