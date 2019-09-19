4835692495ecec7738045a1ac5605dcfc09b9a89
Aidan B <aidan.buchanan@gmail.com>
2019-09-19 12:33:52 -0400

    Initial complete files for Pyman

---
+++ files/Box.py
@@ -0,0 +1,17 @@
+import pygame
+
+from pygame.locals import *
+
+class Box(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        # Call the parent class constructor
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Create a 16x16 surface
+        self.image = pygame.Surface([16, 16])
+        
+        # Set the rectangle's x and y
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
