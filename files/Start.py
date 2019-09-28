d6a668bee43be69b0e8f274ad374de2341fbf9d4
Aidan B <aidan.buchanan@gmail.com>
2019-09-28 16:45:00 -0400

    Added text for the Start & Quit Buttons

--- files/Start.py
+++ files/Start.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 from pygame.locals import *
 
@@ -18,6 +18,9 @@ class Start(object):
         # Initialize window
         window = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
         window.fill(BLACK)
+        
+        # Initialize font
+        font = pygame.font.Font("../font/minecraft.ttf", 36)
 
         # Create Title
         pacman_title = pygame.Surface((384, 192))
@@ -28,21 +31,33 @@ class Start(object):
 
         # Create Start Button
         start = pygame.Surface((192, 48))
-        start.fill(GREEN)
         start_rect = start.get_rect()
         start_rect.centerx = WINDOWWIDTH/2
         start_rect.centery = 320
 
         # Create Exit Button
         quit = pygame.Surface((96, 48))
-        quit.fill(RED)
         quit_rect = quit.get_rect()
         quit_rect.centerx = WINDOWWIDTH/2
         quit_rect.centery = 408
 
         window.blit(pacman_title, pacman_title_rect)
-        window.blit(start, start_rect)
         window.blit(quit, quit_rect)
+        
+        # Create Start Text
+        start_text = font.render("START", True, GREEN)
+        start_text_rect = start_text.get_rect()
+        start_text_rect.centerx = start_rect.centerx
+        start_text_rect.centery = start_rect.centery
+        
+        # Create Quit Text
+        quit_text = font.render("QUIT", True, RED)
+        quit_text_rect = quit_text.get_rect()
+        quit_text_rect.centerx = quit_rect.centerx
+        quit_text_rect.centery = quit_rect.centery
+        
+        window.blit(start_text, start_text_rect)
+        window.blit(quit_text, quit_text_rect)
 
         pygame.display.update()
 
