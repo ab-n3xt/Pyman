d02f72548cee2cc2f7409d1afdd6bdd2136520c4
Aidan B <46637527+ab-n3xt@users.noreply.github.com>
2020-01-21 00:40:51 -0500

    Fixed comments

--- files/constants.py
+++ files/constants.py
@@ -1,5 +1,5 @@
-WINDOWWIDTH = 448 #(16 * 28) (row numbers range from 0 - 27)
-WINDOWHEIGHT = 512 #(16 * 32) (column numbers range from 0 - 31)
+WINDOWWIDTH = 448 # (16 * 28) (row numbers range from 0 - 27)
+WINDOWHEIGHT = 512 # (16 * 32) (column numbers range from 0 - 31)
 SPRITEWIDTH = 16
 SPRITEHEIGHT = 16
 BLACK = (0, 0, 0)
